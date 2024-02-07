9eb51e5474133faa3c1fda11abb57c951253de98
edcan-dev <israel.da.queenie@gmail.com>
2024-02-07 14:30:41 -0600

    je

---
+++ src/ArtAndLoveDRomanApp.tsx
@@ -0,0 +1,10 @@
+export const ArtAndLoveDRomanApp = () => {
+  return (
+    <>
+    <h1>ArtAndLoveDRomanApp</h1>
+    <h2>Starting the App</h2>
+    <h3>Miriam Denessi Román Ordaz</h3>
+    <h4>Painter</h4>
+    </>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { ArtAndLoveDRomanApp } from './ArtAndLoveDRomanApp.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ArtAndLoveDRomanApp />
+  </React.StrictMode>,
+)
